c0a9c9fa511645ea1d8039a90f655d97f50e3c58
NorbertLoh <lohnorbert@gmail.com>
2026-04-04 14:28:24 +0800

    add hmd caption

--- get_predictions.sh
+++ get_predictions.sh
@@ -8,7 +8,7 @@ REMOTE_USER="${REMOTE_USER:-norbert}"
 REMOTE_HOST="${REMOTE_HOST:-xlogin.comp.nus.edu.sg}"
 REMOTE_BASE_DIR="${REMOTE_BASE_DIR:-CS4248/cs4248}"
 
-REMOTE_FILE="~/$REMOTE_BASE_DIR/hateful-captioning/captions_output2.jsonl"
+REMOTE_FILE="~/$REMOTE_BASE_DIR/hateful-captioning/captions_output3.jsonl"
 LOCAL_DIR="./hateful-captioning"
 
 mkdir -p "$LOCAL_DIR"
